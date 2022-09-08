f84f58fc101790bfacd8b20303ea32cc303c1b02
Peter1907 <peter.nady.1408@gmail.com>
2022-09-08 15:56:40 +0200

    Add styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import display from './modules/displayItems.js';
 import activateComments from './modules/comments.js';
 import likes from './modules/countLikes.js';
 import activateReservations from './modules/reservations.js';
+import logo from '../media/house02.png';
+
+const logoImg = document.querySelector('.logo img');
+logoImg.src = logo;
 
 display()
   .then(() => {

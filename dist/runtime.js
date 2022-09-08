d2eee584bacb3a63d942c06197f14ab3a86e3031
Dhuruv Singh <dhuruvs096@gmail.com>
2022-09-08 15:33:44 +0530

    Add likes visited style and Fix error

--- dist/runtime.js
+++ dist/runtime.js
@@ -158,7 +158,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkcapstone_2"] = self["webpackChunkcapstone_2"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkCapstoneJS"] = self["webpackChunkCapstoneJS"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
